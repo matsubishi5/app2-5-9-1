2c4bc6956684d7b73ad1188608ffcedb4688b8b3
TakumiMatsuo <user@matsuotakumi-no-MacBook-Air.local>
2020-03-08 20:08:27 +0900

    [Finish]5-9

--- Bookers/config/initializers/application_controller_renderer.rb
+++ Bookers/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '69249dde7879b8085481dc0bedabc785691d7ab698b68a2909d670c0c070a21d0ad0f8c03ff379b15df5af3f70c7fa33252cb574be79781102b74e3f528729d2'
